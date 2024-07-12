87662149eae585c76e68ae75d59c1e6c2e170760
Alx <alexandreforestier1107@gmail.com>
2024-07-12 03:55:22 +0200

    chore: Add shine animation to button components

--- app/menu/[id]/carte/MenuCard.tsx
+++ app/menu/[id]/carte/MenuCard.tsx
@@ -5,6 +5,7 @@ import { createOrder } from '../actions';
 import OrderSummaryModal from './OrderSummaryModal';
 import QRCode from 'qrcode.react';
 import QRCodeModal from './QRCodeModal';
+import ShineBorder from "@/components/magicui/shine-border";
 
 interface Category {
     id: number;
@@ -128,7 +129,7 @@ export default function MenuCard({ menuId, menuName, categories: initialCategori
                 className="absolute top-4 right-4 bg-gray-200 p-2 rounded-full shadow-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h3v3H3V3zm0 8h3v3H3v-3zm0 8h3v3H3v-3zm8-16h3v3h-3V3zm0 8h3v3h-3v-3zm0 8h3v3h-3v-3zm8-16h3v3h-3V3zm0 8h3v3h-3v-3zm0 8h3v3h-3v-3z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h3v3H3V3zm0 8h3v3H3v-3zm0 8h3v3H3v-3zm8-16h3v3h-3V3zm0 8h3v3h-3V3zm0 8h3v3h-3V3zm8-16h3v3h-3V3zm0 8h3v3h-3V3zm0 8h3v3h-3V3z" />
                 </svg>
             </button>
 
@@ -141,61 +142,72 @@ export default function MenuCard({ menuId, menuName, categories: initialCategori
             ) : (
                 <form onSubmit={handleSubmit} className="space-y-8">
                     {categories.map(category => (
-                        <div key={category.id} className="bg-white p-4 shadow-lg rounded-xl">
-                            <h2 className="text-2xl font-semibold mb-4">{category.name}</h2>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                                {category.items.map(item => (
-                                    <div key={item.id} className="bg-white shadow-md rounded-lg p-4 relative">
-                                        <h3 className="text-lg font-semibold mb-2">{item.name}</h3>
-                                        <div className="flex items-center mt-2">
-                                            <button
-                                                type="button"
-                                                onClick={() => handleDecrement(item.id)}
-                                                className="px-3 py-1 bg-gray-300 rounded-lg"
-                                            >
-                                                -
-                                            </button>
-                                            <input
-                                                id={`quantity-${item.id}`}
-                                                type="number"
-                                                min="0"
-                                                max={item.stock}
-                                                value={orderItems.find(orderItem => orderItem.itemId === item.id)?.quantity || 0}
-                                                onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value))}
-                                                className="mx-2 w-16 px-2 py-1 border border-gray-300 rounded-lg text-center"
-                                            />
-                                            <button
-                                                type="button"
-                                                onClick={() => handleIncrement(item.id)}
-                                                className="px-3 py-1 bg-gray-300 rounded-lg"
-                                            >
-                                                +
-                                            </button>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-                    <div className="bg-white p-6 shadow-lg rounded-2xl">
-                        <label className="block text-gray-800 text-sm font-medium mb-2" htmlFor="user-name">
-                            Nom
-                        </label>
-                        <input
-                            id="user-name"
-                            type="text"
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                            required
-                            className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-                        />
-                        <button
-                            type="submit"
-                            className="mt-4 w-full bg-blue-500 text-white font-medium py-3 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        <ShineBorder
+                            key={category.id}
+                            className="relative flex flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl w-full p-4"
+                            color={["#667db6", "#0082c8", "#667db6"]}
                         >
-                            Passer la commande
-                        </button>
-                    </div>
+                            <div className="bg-white p-4 shadow-lg rounded-xl w-full">
+                                <h2 className="text-2xl font-semibold mb-4">{category.name}</h2>
+                                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                                    {category.items.map(item => (
+                                        <div key={item.id} className="bg-white shadow-md rounded-lg p-4 relative">
+                                            <h3 className="text-lg font-semibold mb-2">{item.name}</h3>
+                                            <div className="flex items-center mt-2">
+                                                <button
+                                                    type="button"
+                                                    onClick={() => handleDecrement(item.id)}
+                                                    className="px-3 py-1 bg-gray-300 rounded-lg"
+                                                >
+                                                    -
+                                                </button>
+                                                <input
+                                                    id={`quantity-${item.id}`}
+                                                    type="number"
+                                                    min="0"
+                                                    max={item.stock}
+                                                    value={orderItems.find(orderItem => orderItem.itemId === item.id)?.quantity || 0}
+                                                    onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value))}
+                                                    className="mx-2 w-16 px-2 py-1 border border-gray-300 rounded-lg text-center"
+                                                />
+                                                <button
+                                                    type="button"
+                                                    onClick={() => handleIncrement(item.id)}
+                                                    className="px-3 py-1 bg-gray-300 rounded-lg"
+                                                >
+                                                    +
+                                                </button>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </ShineBorder>
+                    ))}
+                    <ShineBorder
+                        className="relative flex flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl w-full p-4"
+                            color={["#667db6", "#0082c8", "#667db6"]}
+                    >
+                        <div className="bg-white p-6 shadow-lg rounded-2xl w-full">
+                            <label className="block text-gray-800 text-sm font-medium mb-2" htmlFor="user-name">
+                                Nom
+                            </label>
+                            <input
+                                id="user-name"
+                                type="text"
+                                value={userName}
+                                onChange={(e) => setUserName(e.target.value)}
+                                required
+                                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                            />
+                            <button
+                                type="submit"
+                                className="mt-4 w-full bg-blue-500 text-white font-medium py-3 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                            >
+                                Passer la commande
+                            </button>
+                        </div>
+                    </ShineBorder>
                 </form>
             )}
 
